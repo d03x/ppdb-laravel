92ad1140705365aa6be3ca70c47fa0e38298b21a
dadanhdyt <dadanhidyt@gmail.com>
2024-04-28 19:54:00 +0700

    feat(Template): :zap: Hide dropdown formulir before fill in formulir

--- resources/views/layouts/navigation/index.blade.php
+++ resources/views/layouts/navigation/index.blade.php
@@ -6,6 +6,7 @@
             <x-nav-link icon="pi pi-home">Dashboard</x-nav-link>
             <x-nav-link icon="pi pi-user">Akun</x-nav-link>
             <x-nav-link icon="pi pi-file-edit">Pendaftaran</x-nav-link>
+            @if ($current_user->formulir)
             <x-menu-dropdown name="Pendaftaran" icon="pi pi-file">
                 <x-slot name='dropdown_item'>
                     <x-menu-dropdown-item>Biodata</x-menu-dropdown-item>
@@ -13,6 +14,7 @@
                     <x-menu-dropdown-item>Orang Tua</x-menu-dropdown-item>
                 </x-slot>
             </x-menu-dropdown>
+            @endif
         </ul>
     </div>
 </aside>
